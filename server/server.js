a37cabbd01dda6831dd368adbc0d6cbf49674c04
Ken Takagi <ktakagi@Kens-MBP.hitronhub.home>
2017-03-17 15:05:42 -0700

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const SocketServer = require('ws').Server;
+
+const PORT = 3001;
+
+const server = express()
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', (message) => {
+    wss.clients.forEach(function each(client) {
+      client.send(JSON.stringify(tweet));
+    });
+  });
+
+  ws.on('close', () => {
+  console.log('Client disconnected');
+  });
+});
